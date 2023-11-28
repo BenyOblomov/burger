830c7ef5998902d85383f332b3b35ae6f6b13824
BenyOblomov <benoit.paluch@gmail.com>
2023-11-28 14:55:59 +0100

    footer

--- script.js
+++ script.js
@@ -2,10 +2,14 @@ document.getElementById("burger").addEventListener("click", (event) => {
     document.getElementById("navbar_mob").style.display = "block";
     document.getElementById("navmob").style.display = "none";
     document.getElementById("main2").style.display = "none";
+    document.getElementById("footer").style.display = "none";
+    document.getElementById("titre").style.display = "none";
 })
 
 document.getElementById("quit").addEventListener("click", (event) => {
     document.getElementById("navbar_mob").style.display = "none";
     document.getElementById("navmob").style.display = "flex";
     document.getElementById("main2").style.display = "flex";
+    document.getElementById("footer").style.display = "flex";
+    document.getElementById("titre").style.display = "block";
 })
