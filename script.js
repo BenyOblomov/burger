9b73c66b34a3f728d55e11f494abf86845eb132f
BenyOblomov <benoit.paluch@gmail.com>
2023-11-28 11:05:25 +0100

    burger

---
+++ script.js
@@ -0,0 +1,11 @@
+document.getElementById("burger").addEventListener("click", (event) => {
+    document.getElementById("navbar_mob").style.display = "block";
+    document.getElementById("navmob").style.display = "none";
+    document.getElementById("main2").style.display = "none";
+})
+
+document.getElementById("quit").addEventListener("click", (event) => {
+    document.getElementById("navbar_mob").style.display = "none";
+    document.getElementById("navmob").style.display = "flex";
+    document.getElementById("main2").style.display = "flex";
+})
