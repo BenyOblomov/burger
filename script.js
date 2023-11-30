8b52904903d6eb0ac05b73ea3ebf72e2faa3b716
BenyOblomov <benoit.paluch@gmail.com>
2023-11-30 11:29:29 +0100

    burger

--- script.js
+++ script.js
@@ -1,15 +1,10 @@
 document.getElementById("burger").addEventListener("click", (event) => {
-    document.getElementById("navbar_mob").style.display = "block";
-    document.getElementById("navmob").style.display = "none";
-    document.getElementById("main2").style.display = "none";
-    document.getElementById("footer").style.display = "none";
-    document.getElementById("titre").style.display = "none";
+    document.getElementById("navbar_mob").style.marginLeft = "5%";
+    document.getElementById("slider").style.display = "none";
 })
 
 document.getElementById("quit").addEventListener("click", (event) => {
-    document.getElementById("navbar_mob").style.display = "none";
-    document.getElementById("navmob").style.display = "flex";
-    document.getElementById("main2").style.display = "flex";
-    document.getElementById("footer").style.display = "flex";
-    document.getElementById("titre").style.display = "block";
+    document.getElementById("navbar_mob").style.marginLeft = "-100%";
+    document.getElementById("slider").style.display = "flex";
+
 })
