d5e51aba783628081942f4c9b9f60b50d05b4f0d
BenyOblomov <benoit.paluch@gmail.com>
2023-12-04 10:01:46 +0100

    fix responsive

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-document.getElementById("burger").addEventListener("click", (event) => {
-    document.getElementById("navbar_mob").style.marginLeft = "5%";
-    document.getElementById("slider").style.display = "none";
-})
+// document.getElementById("burger").addEventListener("click", (event) => {
+//     document.getElementById("navbar_mob").style.marginLeft = "5%";
+//     document.getElementById("slider").style.display = "none";
+// })
 
-document.getElementById("quit").addEventListener("click", (event) => {
-    document.getElementById("navbar_mob").style.marginLeft = "-100%";
-    document.getElementById("slider").style.display = "flex";
+// document.getElementById("quit").addEventListener("click", (event) => {
+//     document.getElementById("navbar_mob").style.marginLeft = "-100%";
+//     document.getElementById("slider").style.display = "flex";
 
-})
+// })
